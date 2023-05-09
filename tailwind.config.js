ba1045840e86144b248681137be9364432dc48dc
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-10 00:00:56 +0200

    Make the container smaller

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ export default {
         sans: ['"Inter"', "sans-serif"],
         serif: ['"Noto Serif"', "serif"],
       },
+      container: {
+        screens: {
+          sm: "94vw",
+          md: "90vw",
+          lg: "720px",
+          xl: "960px",
+        },
+      },
     },
   },
   plugins: [],
