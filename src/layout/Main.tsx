5e40fa150e7674c860f04586d9233decf795a3c9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-11 23:32:28 +0200

    Run yarn format

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export function Main({ children }: PropsWithChildren) {
-  return <main className="font-serif container mx-auto relative">{children}</main>;
+  return (
+    <main className="font-serif container mx-auto relative">{children}</main>
+  );
 }
