767fa87db75910e5078ee289e0271c7a06b16984
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-13 19:55:45 +0200

    Fix tailwind precedence bug wrt. conflicting leading-loose and text-xl

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ export function Header() {
     <header className="pt-4 pb-8 md:py-12 text-center leading-loose">
       <h1 className="font-display text-3xl md:text-5xl mt-4">Jimena Oviedo</h1>
       <div className="flex justify-center">
-        <p className="font-subdisplay text-md md:text-xl font-light text-gray-400 leading-loose uppercase">
+        <p className="font-subdisplay leading-loose md:leading-loose text-base md:text-xl font-light text-gray-400 uppercase">
           Breakdown Artist
         </p>
       </div>
