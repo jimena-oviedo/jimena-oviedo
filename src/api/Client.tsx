3ff3086756d0d77c1bd9d47e2d2193e295f8b738
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-13 18:00:37 +0200

    Try to fix env var handling
    Try to debug by adding logs to GH actions

--- src/api/Client.tsx
+++ src/api/Client.tsx
@@ -3,15 +3,18 @@ import { PropsWithChildren, useMemo } from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 export function ClientContextProvider({ children }: PropsWithChildren) {
+  const url = import.meta.env.VITE_GRAPHQL_URL;
+  const token = import.meta.env.VITE_AUTHORIZATION_TOKEN;
+
   const client = useMemo(() => {
+    if (!token) throw "Missing public API token.";
+    if (!url) throw "Missing API URL.";
     return new ApolloClient({
-      uri: import.meta.env.VITE_GRAPHQL_URL,
+      uri: url,
       cache: new InMemoryCache(),
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_AUTHORIZATION_TOKEN}`,
-      },
+      headers: { Authorization: `Bearer ${token}` },
     });
-  }, []);
+  }, [token, url]);
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
